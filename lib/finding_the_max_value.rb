7d9c96dbc2251f7c019beb7280bd66586115890e
Jassiel Rodriguez <hazer94@gmail.com>
2020-08-09 06:32:48 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,9 +1,10 @@
 def find_max_value(array)
   count = 0
-
+  max_value = -1
   while count < array.length do
-    max_value ==  
-    
+   if max_value = count
+     
+  end
     count += 1
   end
 end
